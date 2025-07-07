6ea1c8174716c2be6cb1ca59bbdeabdff3f1dfc6
JHenry <henryjoao4@gmail.com>
2025-07-07 18:25:48 -0300

    mudanças out

--- SpeedTest/speedTest.py
+++ SpeedTest/speedTest.py
@@ -17,7 +17,7 @@ def timer():
         time.sleep(1)
         print(i)
 
-t1 = threading.Thread(target=timer)
+t1 = threading.Thread(target=timer, daemon=True)
 
 
 
@@ -28,24 +28,32 @@ mode = input("Modo de execução (upload, download): ")
 
 
 
+mediada_Byteps = ["B", "kB", "MB", "GB"]
+mediada_bitps = ["bps", "kbps", "Mbps", "Gbps"]
+def getMedida(num_bits, vet_medida):
+    i = 0
+    while num_bits > 1024:
+        num_bits /= 1024
+        i += 1
+    return locale.format_string('%.2f', num_bits, grouping=True) + ' ' + vet_medida[i]
 
 
 def printDataUpload(packet_sent, bytes_sent):
-    print(f"Numero total de bytes: ", locale.format_string('%d', bytes_sent, grouping=True))
-    print(f"Numero total de bits enviados por segundo: ", locale.format_string('%.2f', (8*bytes_sent)/exec_time, grouping=True))
-    print(f"Numero total de pacotes enviados: ", locale.format_string('%d', packet_sent, grouping=True))
-    print(f"Numero total de pacotes enviados por segundo: ", locale.format_string('%.2f', packet_sent/exec_time, grouping=True))
+    print(f"\nNumero total de bytes enviados:", getMedida(bytes_sent, mediada_Byteps))
+    print(f"Numero total de pacotes enviados:", locale.format_string('%d', packet_sent, grouping=True))
+    print(f"Numero total de bits enviados por segundo:", getMedida((8*bytes_sent)/exec_time, mediada_bitps))
+    print(f"Numero total de pacotes enviados por segundo:", locale.format_string('%.2f', packet_sent/exec_time, grouping=True), "\n")
 
 
 
 
-
-def printDataDownload(bytes_recv, pckt_recv, lost_pckt):
-    print(f"Numero total de bytes recebidos:", locale.format_string('%d', bytes_recv, grouping=True))
-    print(f"Numero total de bits recebidos por segundo:", locale.format_string('%.2f', (8*bytes_recv)/exec_time, grouping=True))
+def printDataDownload(bytes_recv, pckt_recv, lost_pckt, non_recv):
+    print(f"\nNumero total de bytes recebidos:", getMedida(bytes_recv, mediada_Byteps))
     print(f"Numero total de pacotes recebidos:", locale.format_string('%d', pckt_recv, grouping=True))
+    print(f"Numero total de bits recebidos por segundo:", getMedida((8*bytes_recv)/exec_time, mediada_bitps))
     print(f"Numero total de pacotes recebidos por segundo:", locale.format_string('%.2f', pckt_recv / exec_time, grouping=True))
     print(f"Numero total de pacotes perdidos:", locale.format_string('%d', lost_pckt, grouping=True))
+    print(f"Numero total de pacotes não recebidos:", locale.format_string('%d', non_recv, grouping=True), "\n")
 
 
 
@@ -64,16 +72,17 @@ def uploadUDP(sock, addr):
         bytes_sent += sock.sendto(b''.join([str(packet_sent).zfill(16).encode(), content]), addr)
         packet_sent += 1
 
+    sock.settimeout(None)
+    msg, _ = sock.recvfrom(500)
+    bytes_recv, pckt_recv, lost_pckt, lenghtS = msg.decode().split("><")
+    sock.sendto(f"{packet_sent}><{bytes_sent}".encode(), addr)
+
     print(f"\nTaxa de UPLOAD nessa maquina({socket.gethostbyname(socket.gethostname())}):")
     printDataUpload(packet_sent, bytes_sent)
 
-    sock.settimeout(None)
-    msg, _ = sock.recvfrom(500)
-    bytes_recv, pckt_recv, lost_pckt = msg.decode().split("><")
-    sock.sendto(f"{packet_sent}><{bytes_sent}".encode(), addr)
-
+    non_recv = packet_sent - int(lenghtS)
     print(f"\nTaxa de DOWNLOAD na outra maquina({host}):")
-    printDataDownload(int(bytes_recv), int(pckt_recv), int(lost_pckt))
+    printDataDownload(int(bytes_recv), int(pckt_recv), int(lost_pckt), non_recv)
 
 
 
@@ -91,15 +100,16 @@ def uploadTCP(sock):
         bytes_sent += sock.send(b''.join([str(packet_sent).zfill(16).encode(), content]))             
         packet_sent += 1
 
+    sock.settimeout(None)
+    bytes_recv, pckt_recv, lost_pckt, lenghtS = sock.recv(500).decode().split("><")
+    sock.send(f"{packet_sent}><{bytes_sent}".encode())
+    
     print(f"\nTaxa de UPLOAD nessa maquina({socket.gethostbyname(socket.gethostname())}):")
     printDataUpload(packet_sent, bytes_sent)
 
-    sock.settimeout(None)
-    bytes_recv, pckt_recv, lost_pckt = sock.recv(500).decode().split("><")
-    sock.send(f"{packet_sent}><{bytes_sent}".encode())
-
+    non_recv = packet_sent - int(lenghtS)
     print(f"\nTaxa de DOWNLOAD na outra maquina({host}):")
-    printDataDownload(int(bytes_recv), int(pckt_recv), int(lost_pckt))
+    printDataDownload(int(bytes_recv), int(pckt_recv), int(lost_pckt), non_recv)
     
 
 
@@ -143,16 +153,18 @@ def downloadUDP(sock):
     pckt_recv = len(v)
     lost_pckt = max(s) - (len(s) - 1)
 
-    print(f"\nTaxa de DOWNLOAD nessa maquina({socket.gethostbyname(socket.gethostname())}):")
-    printDataDownload(bytes_recv, pckt_recv, lost_pckt)
 
     sock.settimeout(None)
-    sock.sendto(f"{bytes_recv}><{pckt_recv}><{lost_pckt}".encode(), addr)
+    sock.sendto(f"{bytes_recv}><{pckt_recv}><{lost_pckt}><{len(s)}".encode(), addr)
     msg, _ = sock.recvfrom(500)
     while b"><" not in msg:
         msg, _ = sock.recvfrom(500)
     try: packet_sent, bytes_sent = msg.decode().split("><")
     except: print(msg.decode())
+
+    non_recv = int(packet_sent) - len(s)
+    print(f"\nTaxa de DOWNLOAD nessa maquina({socket.gethostbyname(socket.gethostname())}):")
+    printDataDownload(bytes_recv, pckt_recv, lost_pckt, non_recv)
 
     print(f"\nTaxa de UPLOAD na outra maquina({host}):")
     printDataUpload(int(packet_sent), int(bytes_sent))
@@ -199,15 +211,16 @@ def downloadTCP(sock):
     pckt_recv = len(v)
     lost_pckt = max(s) - (len(s) - 1)
 
-    print(f"\nTaxa de DOWNLOAD nessa maquina({socket.gethostbyname(socket.gethostname())}):")
-    printDataDownload(bytes_recv, pckt_recv, lost_pckt)
-
     sock.settimeout(None)
-    sock.send(f"{bytes_recv}><{pckt_recv}><{lost_pckt}".encode())
+    sock.send(f"{bytes_recv}><{pckt_recv}><{lost_pckt}><{len(s)}".encode())
     msg = sock.recv(500)
     while b"><" not in msg:
         msg = sock.recv(500)
     packet_sent, bytes_sent = msg.decode().split("><")
+
+    non_recv = int(packet_sent) - len(s)
+    print(f"\nTaxa de DOWNLOAD nessa maquina({socket.gethostbyname(socket.gethostname())}):")
+    printDataDownload(bytes_recv, pckt_recv, lost_pckt, non_recv)
 
     print(f"\nTaxa de UPLOAD na outra maquina({host}):")
     printDataUpload(int(packet_sent), int(bytes_sent))
